d483e15e3e383a5d8fc48afc254ceab0eb5cd1ac
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2021-07-01 12:37:18 +0530

    Add files via upload

---
+++ src/components/columns.js
@@ -0,0 +1,23 @@
+export const COLUMNS =[
+{
+    Header : "Name",
+    accessor: 'name'
+},
+{
+    Header : "Email",
+    accessor: 'email'
+},
+{
+    Header : "Phone Number",
+    accessor: 'number'
+},
+{
+    Header : "Home Address",
+    accessor: 'homeaddress'
+},
+{
+    Header : "Office Address",
+    accessor: 'officeaddress'
+}
+
+]
